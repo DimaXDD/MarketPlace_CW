33c033c64a0388b3da842604d736b05bc9ff92f5
DimaXDD <dimatruba2004@yandex.ru>
2023-05-01 23:47:51 +0300

    version 1.0? (not inst auth)

---
+++ MarketPlace/CourseProject(WPF)/View/VKLoginWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using CourseProject_WPF_.ViewModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CourseProject_WPF_.View
+{
+    public partial class VKLoginWindow : Window
+    {
+        VkLoginViewModel vkLoginViewModel = new VkLoginViewModel();
+
+        public VKLoginWindow()
+        {
+            InitializeComponent();
+            DataContext = vkLoginViewModel;
+        }
+
+        private void authButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (vkLoginViewModel.auth(password.Password))
+                Close();                                  
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
